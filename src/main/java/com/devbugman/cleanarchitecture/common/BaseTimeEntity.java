6ba45e864e0fdf9e9fd1419a1eacf08997d3ab80
devbugman <cksqhr4961@naver.com>
2023-01-15 18:42:56 +0900

    feat: spring-boot project setup

---
+++ src/main/java/com/devbugman/cleanarchitecture/common/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.devbugman.cleanarchitecture.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "create_at", updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedDate;
+}
